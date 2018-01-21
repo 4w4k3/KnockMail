f7a197f8fbe886771a22961c2ad3fa3da954bce4
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-01-20 20:15:08 -0700

    ported to Python 3

--- knock.py
+++ knock.py
@@ -1,70 +1,109 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: iso-8859-15 -*-
 # Copyright 2017 Knock Mail.
 # Written by: * Alisson Moretto - 4w4k3
 # https://github.com/4w4k3/KnockMail
 # Licensed under the BSD-3-Clause
-from os import system
-from sys import stdout, exit
+import os
+import sys
+import argparse
 from validate_email import validate_email
+
+
 results = []
-system('clear')
+
+
+### Argparser
+
+## argparse arguments
+parser = argparse.ArgumentParser(description="e-mail verification utility")
+
+## Scan parser
+parser.add_argument('--email', help='email(s) to scan', metavar='EMAIL')
+parser.add_argument('-f', '--file', help='file with email(s) to check')
+
+args = parser.parse_args()
+
+
 BLUE, RED, WHITE, YELLOW, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;32m', '\033[0m'
+
 def search():
-    inputfil = raw_input("Path to file containing a list of emails: ")
-    with open(inputfil) as inputfile:
-        for line in inputfile:
+    if args.file:
+        inputfile = args.file
+    else:
+        inputfile = input('Path to file with email addresses to scan: ')
+		
+    with open(inputfile) as infile:
+        for line in infile:
             results.append(line.strip())
     count = 0
     for i in results:
         is_valid = validate_email(i,verify=True)
         count += 1
-        if str(is_valid).upper() == "TRUE":
-	    stdout.write(GREEN + "[*] FOUND - [" + i + "] "+ BLUE + "{line " + str(count) + "}\n" + END)
+        if is_valid == True:
+            sys.stdout.write(GREEN + "[*] FOUND - [" + i + "] "+ BLUE + "{line " + str(count) + "}\n" + END)
         else:
-            stdout.write(RED + "[!] NOTFD - [" + i + "]" + BLUE + " {line " + str(count) + "}\n" + END)
+            sys.stdout.write(RED + "[!] NOTFOUND - [" + i + "]" + BLUE + " {line " + str(count) + "}\n" + END)
 
 def single():
-    ema = raw_input("Email address to search: ")
-    is_valid = validate_email(ema,verify=True)
-    if str(is_valid).upper() == "TRUE":
-        stdout.write(GREEN + "[*] FOUND - [" + ema + "]\n" + END)
+    if args.email:
+        ema = str(args.email)
     else:
-	stdout.write(RED + "[!] NOTFD - [" + ema + "]\n" + END)
+        ema = input('Email address to verify: ')
+    is_valid = validate_email(ema,verify=True)
+    if is_valid == True:
+        sys.stdout.write(GREEN + "[*] FOUND - [" + ema + "]\n" + END)
+    else:
+        sys.stdout.write(RED + "[!] NOTFOUND - [" + ema + "]\n" + END)
 
-stdout.write(WHITE + '''
-By: @4w4k3
-https://github.com/4w4k3
-[-                                              -]''' + RED + '''
+def banner():
+    os.system('clear')
+    sys.stdout.write(WHITE + '''
+    By: @4w4k3
+    https://github.com/4w4k3
+    [-                                              -]''' + RED + '''
 
                                                    
- _|    _|                                _|        
- _|  _|    _|_|_|      _|_|      _|_|_|  _|  _|    
- _|_|      _|    _|  _|    _|  _|        _|_|      
- _|  _|    _|    _|  _|    _|  _|        _|  _|    
- _|    _|  _|    _|    _|_|      _|_|_|  _|    _|  
+     _|    _|                                _|        
+     _|  _|    _|_|_|      _|_|      _|_|_|  _|  _|    
+     _|_|      _|    _|  _|    _|  _|        _|_|      
+     _|  _|    _|    _|  _|    _|  _|        _|  _|    
+     _|    _|  _|    _|    _|_|      _|_|_|  _|    _|  
                                                mail''' + END + '''
-            [ ]''' + RED + ' Knock Knock Mail ' + END + '''[ ]
+                [ ]''' + RED + ' Knock Knock Mail ' + END + '''[ ]
 				 	v1.0   
-[-                                              -]
-''')
-print '''
--Usage- Select an option:
+     [-                                              -]
+     ''')
 
-  {0}[{1}1{0}]{1}     Perform a search of emails from specified file.
-  {0}[{1}2{0}]{1}     Single search for specified email.
-  {0}[{1}U{0}]{1}     Update.
-  {0}[{1}E{0}]{1}     Exit.
-'''.format(RED, END)
-while True:
-    try:
-        ask = raw_input("KKM > ").format(RED, END)
-    	if ask.upper() == "1":
-            search()
-	if ask.upper() == "2":
-            single()
-        if ask.upper() == "E":
-            exit(0) 
-    except:
-        print "\nThank you for using Knock Mail."
-        exit(0)
+def menu():
+    print('''
+    -Usage- Select an option:
+
+     {0}[{1}1{0}]{1}     Perform a search of emails from specified file.
+     {0}[{1}2{0}]{1}     Single search for specified email.
+     {0}[{1}E{0}]{1}     Exit.
+    '''.format(RED, END))
+    while True:
+        try:
+            ask = input("KKM > ").format(RED, END)
+            if ask.upper() == "1":
+                search()
+            if ask.upper() == "2":
+                single()
+            if ask.upper() == "E":
+                sys.exit(0) 
+        except:
+            print("\nThank you for using Knock Mail.")
+            sys.exit(0)
+
+def main():
+    if args.email:
+        single()
+    elif args.file:
+        search()
+    else:
+        banner()
+        menu()
+
+if __name__ == '__main__':
+    main()
